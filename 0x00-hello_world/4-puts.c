101426fbdb1d912eb8d1a502938124168d245903
Eseteselase <esete.s.berhanu@gmail.com>
2023-09-07 11:48:20 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hello[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(hello);
+	return (0);
+}
