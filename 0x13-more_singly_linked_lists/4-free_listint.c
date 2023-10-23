e075f678a4f67f7b3613844b721f4b6dfb7bbc6e
Eseteselase <esete.s.berhanu@gmail.com>
2023-10-23 04:54:16 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * Eseteselase Berhanu
+ * free_listint - frees a listint_t list
+ * @head: pointer to head node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
