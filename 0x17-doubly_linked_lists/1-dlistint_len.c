5e211bb080f4cfddb534b9d0ba7653a5e7880027
Eseteselase <esete.s.berhanu@gmail.com>
2023-12-07 12:01:54 -0800

    1.len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,9 @@
-include "lists.h"
+#include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a linked dlistint_t list.
- * @h: List head
- *
- * Return: Number of elements in the list.
+ * dlistint_len - the number of elements in a linked dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
