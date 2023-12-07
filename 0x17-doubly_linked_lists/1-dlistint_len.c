e194277da20941bc5cc3a0847e1c890c8e3ccbaf
Eseteselase <esete.s.berhanu@gmail.com>
2023-12-07 11:59:29 -0800

    1.dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,10 @@ include "lists.h"
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	const dlistint_t *current = h;
+	size_t size = 0;
 
-	while (h)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
+	while (current)
+		size++, current = current->next;
+	return (size);
 }
